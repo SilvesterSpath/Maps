bb792d8f17850c538e041ff838a8c9675d7c97ed
SilvesterSpath <spath.sz@gmail.com>
2023-05-04 07:34:20 +0200

    creating a map div

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 /// <reference types="@types/googlemaps" />
 
-import { User } from './User';
-import { Company } from './Company';
-
-const company = new Company();
-const user = new User();
-
-console.log(company);
-console.log(user);
-
-google;
+new google.maps.Map(document.getElementById('map'), {});
