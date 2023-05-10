338f86d14293921efd60fe7d67c20ad4b516e5dd
SilvesterSpath <spath.sz@gmail.com>
2023-05-10 07:54:33 +0200

    implements interface

--- src/Company.ts
+++ src/Company.ts
@@ -1,12 +1,14 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string;
 
   constructor() {
     this.companyName = faker.company.companyName();
@@ -16,7 +18,6 @@ export class Company {
       lng: parseFloat(faker.address.longitude()),
     };
   }
-  s;
 
   markerContent(): string {
     const content = `
